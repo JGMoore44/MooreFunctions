23633ccbc63f3852537cd8462040c5fd39f22330
James Moore <jmoore17@luc.edu>
2018-10-31 23:36:37 -0500

    Desription File Made

--- mooreFunctionsScript.R
+++ mooreFunctionsScript.R
@@ -1,7 +1,10 @@
 ##Function Script
 install.packages("devtools")
+install.packages("roxygen2")
 library(devtools)
+library(roxygen2)
 
 package.skeleton(list = c("forwardSelection","backElimination","optimumElastic",
                           "setTrainTest","transposeDeIdentify"),
                  name = "mooreFunctions")
+roxygenize("C:/Users/James Moore/Documents/MooreFunctions/mooreFunctions")
